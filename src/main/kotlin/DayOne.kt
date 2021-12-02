7cb7fc11fa3c995d254d0497a6bacb53d85e34a6
Egor Mezentsev <mezencev_e@inbox.ru>
2021-12-02 12:50:56 +0700

    Part 2 of day 1

---
+++ src/main/kotlin/DayOne.kt
@@ -0,0 +1,13 @@
+import java.io.File
+
+object DayOne {
+    private val input = File("/home/egor/Downloads/input.txt").readLines().map { it.toInt() }
+
+    fun partOne(): Int {
+        return input.zipWithNext().count { (first, second) -> first < second }
+    }
+
+    fun partTwo(): Int {
+        return input.windowed(3).map { it.sum() }.zipWithNext().count { (first, second) -> first < second }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,6 @@ import java.io.File
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = File("/home/egor/Downloads/input.txt").readLines().map { it.toInt() }
-        var counter = 0
-        for (i in 1 until input.size)
-            if (input[i - 1] < input[i])
-                counter++
-        print(counter)
+        print(DayOne.partTwo())
     }
 }
